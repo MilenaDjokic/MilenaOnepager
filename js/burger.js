b278ac0a1b18d18bbe75b0b9f21fe117cf2478e7
MilenaDjokic <milena.djudja@gmail.com>
2019-06-26 08:41:24 +0200

    Update Srpski

--- js/burger.js
+++ js/burger.js
@@ -1,7 +1,17 @@
 burger = document.getElementById("burger");
 header = document.getElementById("header");
+navigation = document.getElementsByTagName("nav")[0];
+navItems = navigation.getElementsByTagName("a");
 
-burger.addEventListener("click", () => {
+function toggleNav() {
   burger.classList.toggle("burger-close-btn");
   header.classList.toggle("show-nav");
-});
+}
+
+for (var i = 0; i < navItems.length; i++) {
+  navItems[i].addEventListener("click", toggleNav);
+}
+
+console.log(navigation);
+
+burger.addEventListener("click", toggleNav);
